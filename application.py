2b6cce7b8772c8baebf31f93e6bbdd8e65a9e29c
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-10-08 01:25:33 -0700

    Moved config settings out of application file

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify  
 from flask_socketio import SocketIO 
+from flask_sqlalchemy import SQLAlchemy 
 import eventlet
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config.from_object('settings')
 socketio = SocketIO(app, async_mode='eventlet')
+db = SQLAlchemy(app)
 
 from user import routes 
 from chat import routes 
