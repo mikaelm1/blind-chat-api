937d46a0429403323e58af7f0d1866714a5f5fe8
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-10-08 00:48:31 -0700

    Added chat module to store routes and all related to chat

---
+++ chat/routes.py
@@ -0,0 +1,25 @@
+from flask import jsonify 
+from flask_socketio import emit 
+
+from application import socketio
+
+users = {"user1": "Mike", "user2": "Ashley", "user3": "Becca"}
+
+@socketio.on('connect')
+def connect():
+	print("Client connected")
+	emit('Connected')
+
+@socketio.on('disconnect')
+def disconnect():
+	print("Client disconnected")
+
+def ack():
+	print("Message received")
+
+@socketio.on('connectUser')
+def connect_user(user):
+	print("Connecting user")
+	emit("userList", users)
+
+
