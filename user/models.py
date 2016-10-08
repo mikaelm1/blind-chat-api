502972178a4823579f6104ab0d6df6bbbbe1addc
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-10-08 01:25:46 -0700

    Added user model

---
+++ user/models.py
@@ -0,0 +1,16 @@
+from application import db 
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(100), unique=True)
+	email = db.Column(db.String(50), unique=True)
+	password = db.Column(db.String(80))
+
+	def __init__(self, username, email, password):
+		self.username = username
+		self.email = email
+		self.password = password
+
+
+	def __repr__(self):
+		return "<User %r>" % self.username 
