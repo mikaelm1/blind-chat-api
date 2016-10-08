5c57bd0590c7dcee500741ca1f86d08ac70000b1
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-10-08 02:24:29 -0700

    User register route creates user in db from request args

--- user/routes.py
+++ user/routes.py
@@ -1,8 +1,21 @@
-from flask import jsonify 
+from flask import jsonify, request  
 
-from application import app
+from application import app, db 
+from user.models import User
 
 @app.route('/')
 def root():
 	print("Hitting the root")
 	return jsonify({"result": {"message": "You have reached the root"}})
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	print("Request in register route: " + str(request.form))
+	username = request.form.get('username')
+	email = request.form.get('email')
+	password = request.form.get('password')
+	if username and email and  password:
+		user = User(username, email, password)
+		db.session.add(user)
+		db.session.commit()
+	return "Register route says hi"
