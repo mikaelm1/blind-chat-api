6b3be8ba88f31a624529feacacac16f2ad42796a
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-10-08 00:47:06 -0700

    Reorganized app

--- application.py
+++ application.py
@@ -1,35 +1,10 @@
 from flask import Flask, jsonify  
-from flask_socketio import SocketIO, emit 
+from flask_socketio import SocketIO 
 import eventlet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode='eventlet')
 
-users = ["Mike", "Ben"]
-
-@app.route('/')
-def root():
-	return jsonify({"result": {"message": "You have reached the root"}})
-
-@socketio.on('connect')
-def connect():
-	print("Client connected")
-	emit('Connected')
-
-@socketio.on('disconnect')
-def disconnect():
-	print("Client disconnected")
-
-def ack():
-	print("Message received")
-
-@socketio.on('connectUser')
-def connect_user(user):
-	print("Connecting user")
-	emit("userList", users)
-
-
-
-if __name__ == '__main__':
-	socketio.run(app, debug=True)
+from user import routes 
+from chat import routes 
--- manage.py
+++ manage.py
@@ -1,18 +1,21 @@
 import os, sys 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import flask 
 from flask_script import Manager, Server 
 from flask_migrate import MigrateCommand 
-from application import app 
+from application import app, socketio 
 
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
-manager.add_command("runserver", Server(
-	use_debugger = True,
-	use_reloader = True,
-	host = 'localhost',
-	port = 5000
-	))
+
+@manager.command
+def run():
+    socketio.run(app,
+                 host='localhost',
+                 port=5000,
+                 debug=True,
+                 use_reloader=True)
 
 if __name__ == '__main__':
 	manager.run()
